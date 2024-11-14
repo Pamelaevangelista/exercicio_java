2fc986b4fd6faa413937d2635b5ab50ba32238ec
Pamelaevangelista <panuemanuelecilva@gmail.com>
2024-11-13 21:39:06 -0300

    Add files via upload

---
+++ Treino Java/Exemplo2.java
@@ -0,0 +1,9 @@
+public class Exemplo2
+{
+    public static void main(String[] args) {
+        int variavel = 10 ;
+        String texto = "texto";
+        System.out.println("Valor da variável inteira: " + variavel);
+        System.out.println("Valor da variável texto: " + texto);
+    }
+}
