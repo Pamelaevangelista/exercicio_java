143e856248baae10a85333374045ab9c591a7974
Pamelaevangelista <panuemanuelecilva@gmail.com>
2024-11-14 21:36:31 -0300

    if/else_if/else
    usar o scanner para solicita que o usuário insira a informação

---
+++ Treino Java/ExemploIfElse2 (1).java
@@ -0,0 +1,21 @@
+//Exercicio que verifica o nível de conhecimento de programação com base nos anos de experiência
+// pedir para que o usuário insira a informação
+import java.util.Scanner;
+
+public class ExemploIfElse2 {
+	public static void main (String[] args){
+	    Scanner scanner = new Scanner (System.in);//cria um objeto para ler a entrada do usuário
+	    
+	    System.out.print("Digite quantos anos de experiencia você possui: ");// solicito a entrada do usuário
+	    int anosDeExperiencia = scanner.nextInt();// ler o nº de anos de experiencia
+	    if (anosDeExperiencia <2) { 
+	        System.out.println("Você é um desenvolvedor júnior!");
+	    } else if (anosDeExperiencia >= 2 && anosDeExperiencia <5) {
+	         System.out.println("Você é um desenvolvedor pleno!");
+    	} else{
+    	    System.out.println("Você é um desenvolvedor Sênior!");
+    	}
+    	scanner.close();// fecha o scanner
+    	
+	}
+}
