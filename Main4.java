67d9cc6991d83d7e428a439d0a836e093fd06dc2
Pamelaevangelista <panuemanuelecilva@gmail.com>
2024-11-25 21:02:09 -0300

    aula 2
    carro/pedido

---
+++ Main4.java
@@ -0,0 +1,60 @@
+// Classe Motor
+class Motor {
+    private int potencia;
+    private String tipo;
+
+    public Motor(int potencia, String tipo) {
+        this.potencia = potencia;
+        this.tipo = tipo;
+    }
+
+    public void ligar() {
+        System.out.println("Motor " + tipo + "ligado com potência de " + potencia + "CV." );
+    
+    }
+
+    public void desligar (){
+        System.out.println("Motor " + tipo + "desligado.");
+    }
+    
+    public int getPotencia(){
+        return potencia;
+    }
+
+    public String getTipo(){
+        return tipo;
+    }
+}
+
+// classe carro 
+class Carro {
+    private String modelo;
+    private String cor;
+    private Motor motor;
+
+    public Carro (String modelo, String cor, Motor motor) {
+        this.modelo = modelo;
+        this.cor = cor;
+        this.motor = motor;
+    }
+     public void acelerar(){
+        motor.ligar();
+        System.out.println("O carro " + cor + " " + modelo + " está acelerano com potênca de " + motor.getPotencia() + "CV.");
+    }
+    
+    public void frear() {
+        System.out.println("O carro " + cor + " " + modelo + " está freando."); 
+        motor.desligar();
+    }
+}
+
+// Classe Pricipal
+public class Main4 {
+    public static void main(String[] args) {
+        Motor motor = new Motor(150, "Gasolina");
+        Carro carro = new Carro("fusca", "Azul", motor);
+
+        carro.acelerar();
+        carro.frear();
+    }
+}
