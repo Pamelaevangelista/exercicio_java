4a7c7cf395f83f44055dcbaa5d249219dc8e2178
Pamelaevangelista <panuemanuelecilva@gmail.com>
2024-12-03 20:46:43 -0300

    Delete Exercicio4.java

--- Exercicio4.java
+++
@@ -1,4 +0,0 @@
-/*faça um algoritmo que leia a idade de uma pessoa expressa em dias, meses e 
-anos e escreva a idade dessa pessoa expressa apenas em dias. Considerar ano 
-com 365 dias e mês com 30 dias.*/
-
